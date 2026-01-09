7baea18c86a39c9f231acdfdb5eb716b50ecda9f
Chanyub.Park <mrchypark@gmail.com>
2026-01-09 20:25:37 +0900

    Fix getMaxPageNum with new pagination selectors
    - Use  class to get total page count
    - Old pagination system was replaced with new class-based selectors
    - Returns actual page numbers instead of default 1

--- R/getMaxPageNum.R
+++ R/getMaxPageNum.R
@@ -1,23 +1,42 @@
 #' Get Max Page Number
 #'
+#' Gets the maximum page number for a given category URL.
+#'
 #' @param turl is target url include breakingnews, category url, date without regDate like below.
 #'             'https://news.daum.net/breakingnews/politics/administration?regDate=20220305'
-#' @return Get numeric
+#' @return Get numeric (maximum page number)
 #' @export
-#' @importFrom rvest read_html html_node html_attr
-
+#' @importFrom rvest read_html html_nodes html_text
 getMaxPageNum <- function(turl = url) {
-  hobj <- rvest::read_html(turl)
-  ifnext <- rvest::html_node(hobj, "span.inner_paging a.btn_next")
-  while (class(ifnext) == "xml_node") {
-    nextUrl <- rvest::html_attr(ifnext, "href")
-    nextUrl <- paste0("http://media.daum.net", nextUrl)
-    hobj <- rvest::read_html(nextUrl)
-    ifnext <- rvest::html_node(hobj, "span.inner_paging a.btn_next")
-  }
-  hobj_nodes <- rvest::html_nodes(hobj, "a.num_page")
-  maxPageNum <- rvest::html_text(hobj_nodes)
-  maxPageNum <- as.numeric(maxPageNum)
-  maxPageNum <- maxPageNum[length(maxPageNum)]
-  return(maxPageNum)
+  tryCatch({
+    hobj <- rvest::read_html(turl)
+
+    # Find the total page element (class: total_page)
+    hobj_nodes <- rvest::html_nodes(hobj, ".total_page")
+    if (length(hobj_nodes) > 0) {
+      total_text <- rvest::html_text(hobj_nodes[1])
+      total_pages <- as.numeric(total_text)
+      if (!is.na(total_pages) && total_pages > 0) {
+        return(total_pages)
+      }
+    }
+
+    # Fallback: try to extract from count_page text (e.g., "현재 페이지 1 / 전체 페이지 3")
+    hobj_nodes <- rvest::html_nodes(hobj, ".count_page")
+    if (length(hobj_nodes) > 0) {
+      count_text <- rvest::html_text(hobj_nodes[1])
+      # Extract the last number which is the total pages
+      total_match <- regmatches(count_text, regexpr("[0-9]+$", count_text))
+      if (length(total_match) > 0) {
+        total_pages <- as.numeric(total_match[length(total_match)])
+        if (!is.na(total_pages) && total_pages > 0) {
+          return(total_pages)
+        }
+      }
+    }
+
+    return(1)
+  }, error = function(e) {
+    return(1)
+  })
 }
