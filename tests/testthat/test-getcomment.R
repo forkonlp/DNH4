aa5decfd3020d71efd45af855a893ae5f85ce70c
Chanyub.Park <mrchypark@gmail.com>
2026-01-09 20:25:13 +0900

    Fix testthat API breaking change
    The  syntax no longer works in testthat >= 3.2.0.
    Changed to  for compatibility.

--- tests/testthat/test-getcomment.R
+++ tests/testthat/test-getcomment.R
@@ -9,20 +9,20 @@ with_mock_api({
   test_that("get zero comment work", {
     tar <- "https://news.v.daum.net/v/20180516222212362"
     tem <- getComment(tar)
-    expect(is.null(tem), T)
+    expect_true(is.null(tem))
   })
   
   test_that("get many comment work", {
     tar <- "https://news.v.daum.net/v/20180516162315753"
     tem <- getComment(tar)
     chk <- nrow(tem) > 1
-    expect(chk, T)
+    expect_true(chk)
   })
   
   test_that("get all comment work", {
     tar <- "https://news.v.daum.net/v/20200522093902880"
     tem <- getAllComment(tar)
     chk <- nrow(tem) > 4000
-    expect(chk, T)
+    expect_true(chk)
   })
 })
